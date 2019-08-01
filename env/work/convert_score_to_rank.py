08d73e17a0e657c02a2dbd1833f6b0bc0a769a8e
SzatmariA <ati.szatmari95@gmail.com>
2019-08-01 15:31:27 +0200

    Calculating scores works now

--- env/work/convert_score_to_rank.py
+++ env/work/convert_score_to_rank.py
@@ -9,28 +9,17 @@ from scipy.stats import rankdata
 parser = argparse.ArgumentParser(description='Convert')
 parser.add_argument('-i', '--input', help='Fl score JSON file')
 parser.add_argument('-c', '--csv', help='BinaryDump file')
-parser.add_argument('-b', '--bugfix', help='Fixed bugs csv')
 parser.add_argument('-o', '--output', help='Output csv')
 parser.add_argument('-s', '--score',  help='What score. e.g.: tarantula')
 args = parser.parse_args()
 
 
-def main(inputfile, csvfile, bugfixfile, outputfile, score):
+def main(inputfile, csvfile, outputfile, score):
    print ('Fl score JSON file is "', inputfile)
    print ('Output file is "', outputfile)
    print ('BinaryDump file is', csvfile)
    print ('Score is', score)
    #x = '{"full": {"0": {"ef": 0,"ep": 8,"nf": 1,"np": 12036,"ef/(ef+ep)": 0.0,"nf/(nf+np)": 0.00008307717869901138,"dstar": 0.0,"tarantula": 0.0,"ochiai": 0.0}}}'
-   FixedOnes = []
-
-
-   with open(bugfixfile) as fixed:
-        csv_r = csv.reader(fixed, delimiter=';')
-        charAt = inputfile.find('_');
-        id = inputfile[charAt+1:charAt+2] #That gives us the version number hopefully. (i.e. 3 of Eslint_3_)
-        for row in csv_r:
-                if  row[0] == id:
-                   FixedOnes.append(row[1])
 
    with open(inputfile) as json_file:
         y = json.load(json_file)
@@ -56,7 +45,7 @@ def main(inputfile, csvfile, bugfixfile, outputfile, score):
         line_count = 0
         for row in csv_reader:
                 name = row[1] + ":" + row[2]
-                csv_writer.writerow([name, infection_ranks[str(line_count)], True if name in FixedOnes else False])
+                csv_writer.writerow([name, infection_ranks[str(line_count)]])
                 #print("{};{}".format(name,S[line_count]))
                 line_count += 1
    print('Done.')
