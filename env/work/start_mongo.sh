8b75dacd2f452ce73212558ed52e803525a99b77
Péter Gyimesi <gypeti@users.noreply.github.com>
2019-09-11 15:03:55 +0200

    Fixing old mongodb install

--- env/work/start_mongo.sh
+++ env/work/start_mongo.sh
@@ -2,8 +2,23 @@
 
 ver=$1
 
-ln -s /usr/bin/mongod$ver /usr/bin/mongod
 rm -rf /data/db/*
 rm -rf /var/lib/mongodb/*
-mongod$ver --fork -f /etc/mongodb.conf$ver
 
+case $ver in
+  24)
+    echo "Starting mongodb 2.4"
+    /opt/mongodb-linux-x86_64-2.4.14/bin/mongod --fork -f /work/mongod.conf24
+    ;;
+  26)
+    echo "Starting mongodb 2.6"
+    /opt/mongodb-linux-x86_64-2.6.12/bin/mongod --fork -f /work/mongod.conf26
+    ;;
+  36)
+    echo "Starting mongodb 3.6"
+    /usr/bin/mongod --fork -f /etc/mongod.conf
+    ;;
+  *)
+    echo "Unsupported mongodb version"
+    ;;
+esac
--- env/work/stop_mongo.sh
+++ env/work/stop_mongo.sh
@@ -2,6 +2,21 @@
 
 ver=$1
 
-rm /usr/bin/mongod
-mongod$ver --shutdown -f /etc/mongodb.conf$ver
+case $ver in
+  24)
+    echo "Stopping mongodb 2.4"
+    /opt/mongodb-linux-x86_64-2.4.14/bin/mongod --shutdown -f /work/mongod.conf24
+    ;;
+  26)
+    echo "Stopping mongodb 2.6"
+    /opt/mongodb-linux-x86_64-2.6.12/bin/mongod --shutdown -f /work/mongod.conf26
+    ;;
+  36)
+    echo "Stopping mongodb 3.6"
+    /usr/bin/mongod --shutdown -f /etc/mongod.conf
+    ;;
+  *)
+    echo "Unsupported mongodb version"
+    ;;
+esac
 
